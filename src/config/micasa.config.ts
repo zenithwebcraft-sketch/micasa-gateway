7ef4398331cc1f8870880416a4395f6fb1e76cfc
emarquezg-beep <emarquezg@gmail.com>
2026-01-22 16:20:15 -0500

    FIRST PUSH

--- src/config/micasa.config.ts
+++ src/config/micasa.config.ts
@@ -286,7 +286,7 @@ export const micasaConfig = {
   ],
 
   // ========================
-  // THEME CONFIGURATION
+  // THEME CONFIGURATION TEST
   // ========================
   theme: {
     colors: {
